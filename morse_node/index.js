5aedeab7c22d870459cc37ff07ae34e1bb6c7b37
Hinrich Kaestner <hinrich.kaestner@gmail.com>
2019-06-02 18:52:59 +0200

    Glue code - add IFTTT webhook - implement message queues

--- morse_node/index.js
+++ morse_node/index.js
@@ -1,17 +1,48 @@
-var mqtt = require('mqtt')
-var client = mqtt.connect('mqtt://test.mosquitto.org:1883')
+const mqtt = require('mqtt')
+const request = require('request')
+
+const client = mqtt.connect('mqtt://test.mosquitto.org:1883')
+
+const message_queue = new Object()
+
 
 console.log('Connecting')
 
 client.on('connect', function () {
   client.subscribe('morse/791548')
   console.log("Connected")
-  client.publish('morse/791548', 'hinni,H');
 })
 
 client.on('message', function (topic, message) {
-  // message is Buffer
-  console.log(topic)
-  console.log(message.toString())
-  //client.end()
+  const msg = message.toString()
+  console.log(`${topic}: ${msg}`)
+
+  // CSV -> array
+  const parts = msg.split(',')
+  const sender = parts[0]
+  const letter = parts[1]
+
+  // add to queue
+  if (message_queue.hasOwnProperty(sender)) {
+    message_queue[sender] = `${message_queue[sender]}${letter}`
+  } else {
+    message_queue[sender] = letter
+  }
+
+  // send if STOP string is found
+  if (-1 < message_queue[sender].indexOf("STOP")) {
+    request.post(
+      'https://maker.ifttt.com/trigger/message_received/with/key/ieUjJOEVdcUtlLeoXiOfSpSfHzwnPNq4T5Xz4X33iBR',
+      { json: { value1: sender, value2: message_queue[sender] } },
+      function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          // remove message queue for this sender
+          delete message_queue[sender]
+          console.log(body)
+        }
+        if (error) {
+          console.warn(error)
+        }
+      });
+  }
 })
